aac9aa5bd9dd455a23548b133f59bcf65aa6475f
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-05-01 08:25:54 -0700

    01 - Initial Commit

---
+++ ProjectManhattan/Source/ProjectManhattan/ProjectManhattanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectManhattanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMANHATTAN_API AProjectManhattanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
